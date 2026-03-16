8916c94c7545d9207fab1eea5d13d27f901e6ac4
alva <12345678+alva@users.noreply.github.com>
2026-03-17 01:57:17 +0200

    Test

--- modules/home/features/fcitx5.nix
+++ modules/home/features/fcitx5.nix
@@ -12,15 +12,15 @@
         fcitx5-gtk 
         kdePackages.fcitx5-qt 
       ];
-      settings.inputMethod = {
-        GroupOrder."0" = "Default";
-        "Groups/0" = {
-          Name = "Default";
-          "Default Layout" = "us";
-          DefaultIM = "mozc";
-        };
-        "Groups/0/Items/0".Name = "keyboard-us";
-        "Groups/0/Items/1".Name = "mozc";
+      # settings.inputMethod = {
+        # GroupOrder."0" = "Default";
+        # "Groups/0" = {
+          # Name = "Default";
+          # "Default Layout" = "us";
+          # DefaultIM = "mozc";
+        # };
+        # "Groups/0/Items/0".Name = "keyboard-us";
+        # "Groups/0/Items/1".Name = "mozc";
       };
     };
   };
